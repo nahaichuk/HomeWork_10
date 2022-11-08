7a97e64c2cb6853b1865a1e6a0bc369a0a780ba1
usernamenahaichuk <gely231@mail.com>
2022-11-08 12:29:03 +0200

    HomeWork_10

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace HomeWork_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string[] baza = LoadPhoneBook("Phone.csv");
+            string searchBar = GetSearchBar();
+    
+            string searchResult = "Incorrect input";
+
+            if (searchBar != "")
+            {
+                searchResult = FindASubscriber(searchBar, baza);
+            }
+            Console.WriteLine("search result: " + searchResult);
+        }
+
+        static string[] LoadPhoneBook(string filePhone)
+        {
+            string[] phoneBook = { };
+
+            try
+            {
+                phoneBook = File.ReadAllLines(filePhone);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return phoneBook;
+        }
+
+        static string GetSearchBar()
+        {
+
+            string searchBar = "";
+            Console.WriteLine($"Enter search string:");
+            try 
+            {
+                searchBar = Console.ReadLine(); 
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+
+            return searchBar;
+
+        }
+
+        static string FindASubscriber(string param, string[] data)
+        {
+
+            string result = "The subscriber is not in the phone book.";
+            string[] columnNames = { "name", "surname", "tel" };
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Boolean regexResult = Regex.IsMatch(data[i], param, RegexOptions.IgnoreCase);
+                if (regexResult)
+                {
+                    result = data[i];
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
